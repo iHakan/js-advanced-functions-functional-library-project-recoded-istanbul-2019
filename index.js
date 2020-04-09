96a35d71258b1f6071011184f924c113420f98a4
Hakan Yildiz <hakan@re-coded.com>
2020-04-09 13:49:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(arr1,alert) {
+      arr1.alert;
+        return arr1;
     },
 
     map: function() {
